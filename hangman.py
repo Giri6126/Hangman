55cfcd577f8841f0a9d61f3ec8bd1380fb6d45fa
Giri6126 <girigiri6126@gmail.com>
2025-10-24 08:22:38 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 import random
-#HK
+
 # ---------------- Word Categories & Hints ----------------
 categories = {
     "fruits": ["apple", "banana", "mango", "orange"],
